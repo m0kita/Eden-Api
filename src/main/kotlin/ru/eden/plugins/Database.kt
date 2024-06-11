b6123c16ff2825e16055c2048c66af6f0970376e
m0kita <papanegative@mail.ru>
2024-06-11 19:32:10 +0300

    Add Docker and environment variables

--- src/main/kotlin/ru/eden/plugins/Database.kt
+++ src/main/kotlin/ru/eden/plugins/Database.kt
@@ -4,6 +4,10 @@ import io.ktor.server.application.*
 import org.jetbrains.exposed.sql.Database
 
 fun Application.configureDatabase() {
-    Database.connect("jdbc:postgresql://localhost:12346/test", driver = "org.postgresql.Driver",
-        user = "root", password = "your_pwd")
+    Database.connect(
+        url = environment.config.property("ktor.deployment.databaseURL").getString(),
+        driver = "org.postgresql.Driver",
+        user = environment.config.property("ktor.deployment.databaseUser").getString(),
+        password = environment.config.property("ktor.deployment.databasePassword").getString()
+    )
 }
