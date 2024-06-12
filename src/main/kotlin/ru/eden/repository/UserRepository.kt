1683048985c2c3a3a9fafc8f7de6bb9488e70e0c
m0kita <papanegative@mail.ru>
2024-06-12 13:44:58 +0300

    Fix registration and login

--- src/main/kotlin/ru/eden/repository/UserRepository.kt
+++ src/main/kotlin/ru/eden/repository/UserRepository.kt
@@ -3,7 +3,7 @@ package ru.eden.repository
 import ru.eden.model.User
 
 interface UserRepository {
-    suspend fun addUser(user: User)
+    suspend fun addUser(user: User): Boolean
 
     suspend fun userByEmailAndPassword(email: String, password: String) : User?
 }
