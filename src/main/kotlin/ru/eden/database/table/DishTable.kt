2d2798aff34c48652b314443afca7591876d999e
m0kita <papanegative@mail.ru>
2024-06-15 17:12:12 +0300

    Длина описания

--- src/main/kotlin/ru/eden/database/table/DishTable.kt
+++ src/main/kotlin/ru/eden/database/table/DishTable.kt
@@ -6,7 +6,7 @@ object DishTable : IntIdTable("dish") {
     val type = integer("type")
     val imageURL = varchar("imageURL", 200)
     val name = varchar("name", 50)
-    val description = varchar("description", 100)
+    val description = varchar("description", 200)
     val price = integer("price")
     val isAvailable = bool("isAvailable")
 }
